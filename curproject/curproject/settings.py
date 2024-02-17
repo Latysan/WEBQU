198da6cba763c49ea6f837b479f15dbfc14e64bd
لطيفة سند <laty2548@gmail.com>
2024-02-17 10:49:08 +0300

    Set up initial project structure and configuration settings

--- curproject/curproject/settings.py
+++ curproject/curproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'myapp',
 ]
 
 MIDDLEWARE = [
@@ -47,14 +49,22 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
+
+    
 ]
 
 ROOT_URLCONF = 'curproject.urls'
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, "myapp" + os.sep + "templates") 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL  = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ curproject/myapp/currencymodule/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from myapp.currencymodule import views
+
+urlpatterns = [    
+     path('',views.index, name="index"),
+]
